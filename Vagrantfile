3c0f68ac51f4318fd4d93350f6f8bd4cc5fde095
Nils Vogt <hello.nils.vogt@gmail.com>
2017-07-17 08:50:30 +0200

    add provisioner puppet

--- Vagrantfile
+++ Vagrantfile
@@ -3,7 +3,8 @@
 
 Vagrant.configure("2") do |config|
   config.vm.box = "ubuntu/trusty64"
-  config.vm.provision :shell, path: "box-settings/bootstrap.sh", keep_color: true
+  #config.vm.provision :shell, path: "provisioning/shell/bootstrap.sh", keep_color: true
+  config.vm.provision :puppet, manifests_path: "provisioning/puppet/environments/development/manifests", module_path: "provisioning/puppet/modules"
   config.vm.network "forwarded_port", guest: 80,    host: 8082
   config.vm.network "forwarded_port", guest: 3306,  host: 33306
   config.vm.network "private_network", ip: "111.111.11.11"
@@ -14,5 +15,11 @@ Vagrant.configure("2") do |config|
     v.customize ["modifyvm", :id, "--cpuexecutioncap", "95"]
     v.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
     v.customize ["modifyvm", :id, "--natdnsproxy1", "on"]
+  end
+  config.vm.provision "puppet" do |puppet|
+    puppet.facter = {
+      'fqdn'             => "foo",
+    }
+    puppet.manifests_path = "provisioning/puppet/environments/development/manifests"
   end
 end
---
+++ provisioning/puppet/environments/development/manifests/default.pp
@@ -0,0 +1,54 @@
+# see https://docs.puppet.com/puppet/latest/type.html for documentation of
+# available recource type documentation
+
+# update apt
+exec { 'apt-get update':
+  path => '/usr/bin', # The search path used for command execution
+}
+
+# install php7
+exec { 'php-repo':
+  #  The actual command to execute.
+  command => 'sudo apt-get install -y software-properties-common>/dev/null && sudo add-apt-repository -y ppa:ondrej/php>/dev/null && sudo apt-get update>/dev/null',
+  path => '/usr/bin',
+}
+
+$php7packages = [
+  'php7.0-fpm', 'php7.0', 'php7.0-mysql',
+  'php7.0-curl', 'php7.0-gd', 'php7.0-intl',
+  'php-pear', 'php7.0-imap', 'php7.0-mcrypt',
+  'php7.0-sqlite3', 'php7.0-mbstring', 'php7.0-bcmath', 'snmp'
+]
+
+package { $php7packages: # install multiple packages at once
+  ensure  => present, # present aka installed|absent|purged|held|latest
+  require => Exec['php-repo'], # One or more resources that this resource depends on
+}
+
+# install and run nginx
+package { 'nginx':
+  ensure  => present, # Whether the file should exist (present|absent|file|directory|link)
+  require => Exec['apt-get update'],
+}
+
+service { 'nginx':
+  ensure  => 'running', # stopped aka false | running aka true.
+  require => Package['nginx'],
+}
+
+# link shipped nginx-config
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'link', # present|absent|file|directory|link.
+  target  => '/home/vagrant/code/provisioning/shell/nginx.default', # symlink target
+  require => Package['nginx'],
+  # If Puppet makes changes to this resource, it will cause all of the notified resources to refresh. (Refresh behavior varies by resource type: services will restart, mounts will unmount and re-mount, etc. Not all types can refresh.)
+  notify  => Service['nginx'],
+}
+
+# prepare the public root
+file { '/usr/share/nginx/html':
+  ensure  => 'link',
+  target  => '/vagrant/app/public',
+  require => Package['nginx'],
+  force => true,
+}
